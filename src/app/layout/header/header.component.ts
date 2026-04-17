2dca50742851584c82e13299ba8ee49dfad109c7
Redfenzi <116824735+Redfenzi@users.noreply.github.com>
2026-04-17 20:56:00 +0200

    fix: header iOS - top:0 evite double safe-area-inset-top

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -62,7 +62,7 @@ import { TranslateModule } from '@ngx-translate/core';
       justify-content: space-between;
       padding: 0 32px;
       position: sticky;
-      top: env(safe-area-inset-top, 0px);
+      top: 0;
       z-index: 100;
     }
 
